b0f58c4ee41e631aac9cbcd1f73c9e4ef308954c
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2020-06-02 22:39:38 -0700

    - finished modeling example next - review docs - create new release

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,4 +1,54 @@
 //! Optimization algebraic modeling tools.
+//!
+//! These fools facilitate the construction, solution, and extractio of results
+//! of optimization models.
+//! 
+//! ## Example
+//! ```
+//! use numopt::model::*;
+//! use numopt::solver::*;
+//! 
+//! // Variables
+//! let x = VariableScalar::new_continuous("x");
+//! let y = VariableScalar::new_continuous("y");
+//!
+//! // Objective function
+//! let f = 2.*&x - 5.*&y;
+//! 
+//! // Constraints
+//! let c1 = x.geq(100.);
+//! let c2 = x.leq(200.);
+//! let c3 = y.geq(80.);
+//! let c4 = y.leq(170.);
+//! let c5 = (&y + 2.*&x).equal(&x + 200.);
+//! 
+//! // Model
+//! let mut m = Model::new();
+//! m.set_objective(Objective::minimize(&f));
+//! m.add_constraint(&c1);
+//! m.add_constraint(&c2);
+//! m.add_constraint(&c3);
+//! m.add_constraint(&c4);
+//! m.add_constraint(&c5);
+//! 
+//! // Solver
+//! let mut s = SolverClpCmd::new();
+//! s.set_param("logLevel", SolverParam::IntParam(0)).unwrap();
+//! m.solve(&s).unwrap();
+//! 
+//! // Status
+//! assert_eq!(*m.solver_status().unwrap(), SolverStatus::Solved);
+//! 
+//! // Primal results
+//! let final_primals = m.final_primals();
+//! assert_eq!(*final_primals.get(&x).unwrap(), 100.);
+//! assert_eq!(f.evaluate(&final_primals), -300.);
+//! 
+//! // Dual results
+//! let final_duals = m.final_duals();
+//! assert_eq!(*final_duals.get(&c1).unwrap(), 7.);
+//! assert_eq!(*final_duals.get(&c5).unwrap(), -5.);
+//! ```
 
 pub mod node;
 pub mod node_base;
